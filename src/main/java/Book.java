c734eb8e28657e73a689ce1c6735ce7ac828bbab
Aresky Berkane <areskyberkane@gmail.com>
2020-01-07 16:31:23 +0000

    Added constructor to Book class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,12 @@
 public class Book {
+    private String name;
+    private String author;
+    private String genre;
+
+    public Book(String name, String author, String genre){
+        this.author = author;
+        this.name = name;
+        this.genre = genre;
+    }
+
 }
