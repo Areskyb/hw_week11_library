fbe8bd1582b78a6dde46df259ca0fbd21c38215a
Aresky Berkane <areskyberkane@gmail.com>
2020-01-07 16:39:58 +0000

    Write test for counting total books in library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,6 @@
 public class Library {
+
+    public int numberOfBooks() {
+
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Book book;
+    private Book book2;
+    private Book book3;
+    private Library library;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book('Lord of the Rings', 'yo mama', 'Real');
+        book2 = new Book('Starlight','anonymous','metal');
+        book3 = new Book('no','yes','female');
+    }
+
+    @Test
+    public void hasNumberOfBooks(){
+        assertEquals(3,library.numberOfBooks());
+    }
+
 }
