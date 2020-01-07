812ce2c7e398a4b49d8d5a9dc7a4451343f1b5e3
Aresky Berkane <areskyberkane@gmail.com>
2020-01-07 16:48:00 +0000

    Test is passing for count total of books

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Library {
     private ArrayList<Book> books;
 
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+
 
 
     public int numberOfBooks() {
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -13,13 +13,17 @@ public class LibraryTest {
     @Before
     public void before(){
         library = new Library();
-        book = new Book('Lord of the Rings', 'yo mama', 'Real');
-        book2 = new Book('Starlight','anonymous','metal');
-        book3 = new Book('no','yes','female');
+        book = new Book("Lord of the Rings", "yo mama", "Real");
+        book2 = new Book("Starlight","anonymous","metal");
+        book3 = new Book("no","yes","female");
     }
 
     @Test
     public void hasNumberOfBooks(){
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+
         assertEquals(3,library.numberOfBooks());
     }
 
