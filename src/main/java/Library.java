e95e849ba69be129cbdb12864bfa85523ebe3446
Aresky Berkane <areskyberkane@gmail.com>
2020-01-07 16:43:37 +0000

    Add addBooks and countTotalOfBooks methods

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+
 
     public int numberOfBooks() {
+        return this.books.size();
+    }
 
+    public void addBook(Book book) {
+        this.books.add(book);
     }
 }
